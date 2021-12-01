f678118681320980c4c6d1b83949c9663e66a7be
godhyeongman <88314186+godhyeongman@users.noreply.github.com>
2021-12-02 06:16:59 +0900

    입출력 성공
    몇시간 걸렸는지 가늠도 안옴 새벽 6시에 겨우 자다 깨서 실험 성공

--- practiceNode.js
+++ practiceNode.js
@@ -4,11 +4,31 @@ const rl = readline.createInterface({
   output: process.stdout,
 });
 
-let input = [];
+class Please {
+  constructor() {
+    this.tool = {};
+    this.start();
+  }
+  start() {
+    let word;
+    rl.on("line", (line) => {
+      word = line; // 한 줄씩 입력받은 후 실행할 코드
+      rl.close(); // 필수. 없으면 입력을 무한히 받는다
+    });
+    rl.on("close", () => {
+      this.copyWord(word); // 입력이 끝난 후 실행할 코드
+    });
+  }
 
-rl.on("line", function (line) {
-  input.push(line);
-}).on("close", function () {
-  console.log(input);
-  process.exit();
-});
+  copyWord(word) {
+    console.log(`입력받은값${word}`);
+    const copyWordArr = Array.from(word);
+    console.log(`배열로 얕은복사 ${copyWordArr}`);
+    console.log(`다시문자열로 ${copyWordArr.join("")}`);
+    console.log(`이건 타입이 뭘까? ${typeof copyWordArr.join("")}`);
+    console.log(`다시 쪼개뿔기 ${copyWordArr.join("")}`);
+    console.log(copyWordArr, word);
+  }
+}
+
+new Please();
