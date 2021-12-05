f49dc3a5354b83c8ef8dc31532555f864836fc72
godhyeongman <88314186+godhyeongman@users.noreply.github.com>
2021-12-05 18:35:12 +0900

    테스트 전날 마지막 공부
    클래스로 쓰면 들여쓰기가 1개 추가됨 이를 방지하기위해 객체리터럴로 listStorage라는 객체를 생성후 모든함수를
    listStorage.함수이름 = function(){} 이런실으로 함수표현식으로 전환하여 동작함
    동작 결과 성공함 시험전 마지막으로 배열 메서드들의 특징과 Object메서드들 복습,for..in/of복습함

--- Obj.js
+++ Obj.js
@@ -8,56 +8,140 @@ const rl = readline.createInterface({
 // ctrl + home,end = 페이지 맨끝 혹은 맨첫번째
 
 // 객채 정보저장 idea
-// const listStorage = {
-//   todo: {
-//     id1: [
-//       /*정보입력*/
-//     ],
-//     id2: function blahBlah() {},
-//   },
-// };
+const listStorage = {
+  idCount: 0,
+  task: [],
+};
 
-function start() {
+listStorage.start = function () {
   console.log("필기를 시작합니다.");
-  getUserInput();
-}
+  this.getUserInput();
+};
 
-function getUserInput() {
+listStorage.getUserInput = function () {
   let userInput;
+  const that = this;
   rl.question("", (answer) => {
     userInput = answer;
-    separateInput(userInput);
+    that.separateInput(userInput);
   });
-}
+};
 
-function separateInput(input) {
-  const copiedUserInput = Array.from(input.trim());
-  const separtedUserInput = copiedUserInput.split($);
-  checkWhatToDo(separtedUserInput);
-}
+listStorage.separateInput = function (input) {
+  const copiedUserInput = input;
+  const separtedUserInput = copiedUserInput.trim().split("$");
+  //띄어쓰기 코드 분리된 배열을 돌면서 join('') 하면됨
+  this.checkWhatToDo(separtedUserInput);
+};
 
-function checkWhatToDo(input) {
+listStorage.checkWhatToDo = function (input) {
   const check = input[0];
-  checkError(input);
+  if (this.checkError(input)) {
+    return;
+  }
   if (check === "add") {
+    this.addTask(input);
   }
   if (check === "show") {
+    this.showTask(input);
   }
-  if (check === "done") {
-  }
-}
 
-function checkError() {
+  if (check === "update") {
+    this.updateTask(input);
+  }
+};
+
+listStorage.checkError = function (input) {
   if (input.length > 3) {
     console.log("너무큰 입력값입니다.");
-    getUserInput();
+    this.getUserInput();
     return;
   }
-  if (!["add", "show", "done"].includes(input[0])) {
-    console.log("add, show, done 3가지 기능만을 지원합니다 입력을 확인하세요");
-    getUserInput();
+  if (!["add", "show", "update"].includes(input[0])) {
+    console.log(
+      "add, show, update 3가지 기능만을 지원합니다 입력을 확인하세요"
+    );
+    this.getUserInput();
     return;
   }
-}
+};
 
-start();
+listStorage.addTask = function (input) {
+  listStorage.idCount++;
+  listStorage.task.push({
+    id: listStorage.idCount,
+    task: input[1],
+    time: null,
+    statement: "todo",
+  });
+  this.checkCurrentStateMent();
+  this.getUserInput();
+};
+
+listStorage.checkCurrentStateMent = function () {
+  let toDo = 0;
+  let doing = 0;
+  let done = 0;
+
+  listStorage.task.forEach((item) => {
+    if (item.statement === "todo") {
+      toDo++;
+    } else if (item.statement === "doing") {
+      doing++;
+    } else if (item.statement === "done") {
+      done++;
+    }
+  });
+  console.log(`현재상태: todo:${toDo}, doing:${doing}, done:${done}`);
+};
+
+listStorage.showTask = function (input) {
+  const userWantedTask = input[1];
+  const getUWTfromStorage = [];
+
+  listStorage.task.forEach((item) => {
+    if (item.statement === userWantedTask) {
+      getUWTfromStorage.push([item.id, item.task]);
+    }
+  }); // 유저입력 task를 listStorage에서 확인후 getUWt[]에 id와task만 저장
+
+  console.log(getUWTfromStorage.flat());
+  this.getUserInput();
+};
+
+listStorage.updateTask = function (input) {
+  const id = +input[1];
+  const willChangeTask = input[2];
+  this.willChangeTask();
+
+  listStorage.task.forEach((item) => {
+    if (item.id === id) {
+      item.statement = willChangeTask;
+      this.checkChangedDoing(willChangeTask, item);
+      this.checkChangedDone(willChangeTask, item);
+    }
+  });
+  this.checkCurrentStateMent();
+  this.getUserInput();
+};
+
+listStorage.checkWillChangeTask = function (willChangeTask) {
+  if (["todo", "doing", "done"].includes(willChangeTask)) {
+    console.log("바꿔주고 싶은 상태에 오류가 있습니다.");
+    this.getUserInput();
+  }
+};
+
+listStorage.checkChangedDoing = function (willChangeTask, item) {
+  if (willChangeTask === "doing") {
+    item.time = new Date();
+  }
+};
+listStorage.checkChangedDone = function (willChangeTask, item) {
+  if (willChangeTask === "done") {
+    const endTime = new Date();
+    const startTime = item.time;
+    item.time = Math.floor(endTime - startTime) / 1000;
+  }
+};
+listStorage.start();
