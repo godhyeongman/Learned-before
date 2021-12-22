f265cf486c3bf2254915b24349c649794a28561c
godhyeongman <88314186+godhyeongman@users.noreply.github.com>
2021-12-22 17:42:28 +0900

    도형 넓이 구하기 함수
    미션 요구사항 구현 완료
    부족한점
    
    arguments 사용시 화삻표 함수를 사용하니 오류 생성되어 함수선언으로 변경함
    추측은 화살표 함수의 속성중 모르는 점이 있었다고 생각됨

--- functionPr.js
+++ functionPr.js
@@ -1,14 +1,41 @@
 // 반지름을 통해 원의 넓이 리턴
-circle = (half) => half * half * Math.PI;
+const circle = (half) => {
+  if (isNaN(half)) {
+    return console.log("error");
+  }
+  return half * half * Math.PI;
+};
 
 // 인자를 입력받아 사각형의 넓이를 리턴
-rectangle = (width, height) => width * height;
+const rectangle = (width, height) => {
+  if (checkParameter(width, height)) return;
+
+  return width * height;
+};
 
 // 인자를 입력받아 사다리꼴 넓이를 리턴
-trapezoid = (topLength, bottomLength, height) =>
-  ((topLength + bottomLength) * height) / 2;
+const trapezoid = (topLength, bottomLength, height) => {
+  if (checkParameter(topLength, bottomLength, height)) return;
+  return ((topLength + bottomLength) * height) / 2;
+};
 
 // 인자를 입력받아 원기둥의 넓이를 리턴
-cylinder = (half, height) => {
+const cylinder = (half, height) => {
+  if (checkParameter(half, height)) return;
   return circle(half) * 2 + 2 * Math.PI * half * height;
 };
+
+function checkParameter() {
+  for (let i = 0; i < arguments.length; i++) {
+    if (isNaN(arguments[i]) || !arguments[i]) {
+      console.log("error");
+      return true;
+    }
+  }
+}
+// console.log(circle(3));
+// console.log(rectangle(1, 3));
+// console.log(trapezoid(1, 3, 7));
+// console.log(cylinder(1, 3));
+
+// 테스트 결과 오류 검증 O, 계산 결과 출력 O
