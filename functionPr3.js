7f2d8d823233a429007435c83dd7b5543d1060d7
godhyeongman <aaaa4000@naver.com>
2021-12-24 16:58:20 +0900

    스텝 4  mission 2
    재귀 알고리즘 사용하여 문제 해결함
    
    1. 객체안에 childnode를 확인함수, type을 확인함수 생성
    2. 객체안에 childnode가 존재하면 다시 확인함수 호출
    
    결과 정상출력

--- functionPr3.js
+++ functionPr3.js
@@ -41,4 +41,110 @@ function filteringNum() {
   console.log(`중복 제거: ${filterdNumData}`);
 }
 
-filteringNum();
+// filteringNum();
+
+const check = [
+  {
+    id: 1,
+    name: "Yong",
+    phone: "010-0000-0000",
+    type: "sk",
+    childnode: [
+      {
+        id: 11,
+        name: "echo",
+        phone: "010-0000-1111",
+        type: "kt",
+        childnode: [
+          {
+            id: 115,
+            name: "hary",
+            phone: "211-1111-0000",
+            type: "sk",
+            childnode: [
+              {
+                id: 1159,
+                name: "pobi",
+                phone: "010-444-000",
+                type: "kt",
+                childnode: [
+                  {
+                    id: 11592,
+                    name: "cherry",
+                    phone: "111-222-0000",
+                    type: "lg",
+                    childnode: [],
+                  },
+                  {
+                    id: 11595,
+                    name: "solvin",
+                    phone: "010-000-3333",
+                    type: "sk",
+                    childnode: [],
+                  },
+                ],
+              },
+            ],
+          },
+          {
+            id: 116,
+            name: "kim",
+            phone: "444-111-0200",
+            type: "kt",
+            childnode: [
+              {
+                id: 1168,
+                name: "hani",
+                phone: "010-222-0000",
+                type: "sk",
+                childnode: [
+                  {
+                    id: 11689,
+                    name: "ho",
+                    phone: "010-000-0000",
+                    type: "kt",
+                    childnode: [
+                      {
+                        id: 116890,
+                        name: "wonsuk",
+                        phone: "010-000-0000",
+                        type: "kt",
+                        childnode: [],
+                      },
+                      {
+                        id: 1168901,
+                        name: "chulsu",
+                        phone: "010-0000-0000",
+                        type: "sk",
+                        childnode: [],
+                      },
+                    ],
+                  },
+                ],
+              },
+            ],
+          },
+          {
+            id: 117,
+            name: "hong",
+            phone: "010-0000-0000",
+            type: "lg",
+            childnode: [],
+          },
+        ],
+      },
+    ],
+  },
+];
+const answer = [];
+function checkType(obj) {
+  if (obj.type === "sk") answer.push(obj.name);
+}
+function checkChildNode(obj) {
+  if (obj.childnode) {
+    checkType(obj);
+    obj.childnode.forEach((item) => checkChildNode(item));
+  }
+}
+checkChildNode(check[0]);
+console.log(answer);
